4d010072bbfa99cccd1c8e6856151e74413df40e
Maksim Goydin <maksim.gojdin@gmail.com>
2025-11-24 00:50:13 +0200

    Solving

---
+++ main.py
@@ -0,0 +1,25 @@
+def check_brackets(s):
+    stack = []
+    bracket_map = {')': '(', '}': '{', ']': '['}
+    
+    for char in s:
+        if char in "({[":
+            stack.append(char)
+        elif char in ")}]":
+            if not stack or stack[-1] != bracket_map[char]:
+                return False
+            stack.pop()
+    
+    return len(stack) == 0
+
+def main():
+    print("Введите строку:")
+    user_input = input().strip()
+    
+    if check_brackets(user_input):
+        print("Строка существует")
+    else:
+        print("Строка не существует")
+
+if __name__ == "__main__":
+    main()
